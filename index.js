dea38fa8b9c4631f286410e190713e2b92963eab
2200704780 <145361541+2200704780@users.noreply.github.com>
2024-02-05 21:03:52 +0300

    second

---
+++ index.js
@@ -0,0 +1,100 @@
+// let item1 = "Apples";
+// let item2 = "Pineapples"
+// console.log(item1);
+// console.log(item2);
+
+// let item3 = 'Mango';
+// console.log(item3);
+  
+
+
+// let fruits =['Apples','Pineapples','Mango'];
+// console.log(fruits);
+// let names = ['ivan','irene','joan']
+   // MUTATING FUNCTIONS
+//console.log(names[names.length -1]);
+// names.push("kalwaza");
+// console.log(names);
+// names.pop();
+// console.log(names);
+// let boys = ['ivan','irene','joan']
+// boys.slice(1);
+// console.log(boys);
+ 
+// NON MUTATING FUNCTIONS
+// let bbcMarks =[90, 60, 50, 86];
+//  ///.filter function
+//  let filteredBbcMarks = bbcMarks.filter((element) =>{
+//     console.log(element);
+//     return true;
+//  });
+//  console.log(filteredBbcMarks);
+// let bbcMarks =[90, 60, 30, 15];
+// //  ///.filter function
+//   let filteredBbcMarks = bbcMarks.filter((element) =>{
+//     console.log(element);
+//     if(element<=50){
+// return true
+//     };
+//   });
+//  console.log(filteredBbcMarks);
+// let bbcStudents =['ivan','joan','brian']
+// let filteredBbcStudents =bbcStudents.filter((element) =>{
+//     console.log(element);
+//     if(element !='ivan'){
+// return true;
+//     }
+// })
+// console.log(filteredBbcStudents);
+// let bbcStudents =['50' ,'joan','brian']
+// let filteredBbcStudents =bbcStudents.filter((element) =>{
+//     console.log(element);
+//     if(element ===50){
+// return true;
+//     }
+// })
+// console.log(filteredBbcStudents);
+
+
+
+
+            //CLASS EXERCISE
+// let bbcStudents =['1.Ivan','2.Rollins','3.Irene','4.Joan','5.Winnie','6.Gideon', '7.Lincoln','8.Christopher','9.Anthony'];
+// let filteredbbcStudents= bbcStudents.filter((element) =>{
+//     console.log(element);
+//     if (element !='1.Ivan'){
+//     return true;
+//     }
+// })
+ 
+// console.log(filteredbbcStudents);
+
+//CHANGING  AN ARRAY ELEMENT
+// const cars =['Volvo','bmw','mazda','bentely'];
+// cars [1]= 'macedese benz'
+// console.log(cars);
+
+
+  // CONVERTING AN ARRAY TO A STRING
+//   const fruits =['Banana','Orange','Apple','Watermelon','Mango']
+//   let filteredfruits= fruits.toString();
+//   console.log(filteredfruits);
+
+//  const fruits =['Banana','Orange','Apple','Watermelon','Mango']
+//   let filteredfruits= fruits.unshift();
+//   console.log(filteredfruits);
+ 
+// let array1 =['ivan', 'joan'];
+// let array2 =['rollins', 'irene'];
+// let array3 =['gideon', 'winnie'];
+//    let myFriends =array1.concat(array2, array3);
+//    console.log(myFriends);
+// let name = 'ivan ssembeguya';
+// console.log(name);
+// let person ={
+//    name:'ivan',
+//    age: 24
+// };
+// console.log(person);
+  person.name='ivan';
+console.log(person.name);
